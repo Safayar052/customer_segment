3fc38baf46980b062e6b88da5363a87b9fb9fabf
sahmed052 <safayar@sybrandsplace.nl>
2024-04-04 16:12:44 +0200

    update main to read specific column for customer number

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Customer Segment SQL Generator
-This command line executable takes an Excel file containing customer numbers and their corresponding customer segment ID as input and generates an SQL file containing insert statements to assign customer numbers to Intershop customer segments. The output SQL file will be named after the customer segment ID.
+This command line executable takes an Excel file containing customer numbers, cloumn header name for customer number and their corresponding customer segment ID as input and generates an SQL file containing insert statements to assign customer numbers to Intershop customer segments. The output SQL file will be named after the customer segment ID.
 
 ## Setup
 - Rust : https://www.rust-lang.org/tools/install
@@ -23,17 +23,18 @@ cargo build --release
 
 ## Usage
 ```sh
-./customer_segment <excel_file> <customer_segment_id>
+./customer_segment <excel_file> <column_header_name> <customer_segment_id>
 ```
 
 <excel_file>: Path to the Excel file containing customer numbers and segment IDs.
+<column_header_name>: Column header for the customer number
 <customer_segment_id>: The ID of the customer segment. This will be used to name the output SQL file.
 
 ## Example
 Suppose we have an Excel file named customer_data.xlsx containing customer numbers and segment IDs, and we want to generate SQL statements for the customer segment with ID CG_BB_A7_Werkzaamheden. We can use the following command:
 
 ```sh
-./customer_segment customer_data.xlsx CG_BB_A7_Werkzaamheden
+./customer_segment customer_data.xlsx Klantnummer CG_BB_A7_Werkzaamheden
 ```
 
 This will generate an SQL file named CG_BB_A7_Werkzaamheden.sql containing insert statements for assigning customer numbers to the CG_BB_A7_Werkzaamheden customer segment.
--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ fn main() {
     // Get the command-line arguments
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 3 {
-        eprintln!("Usage: {} <arg1> <arg2>, excel file name and customer segment id are missing", args[0]);
+    if args.len() < 4 {
+        eprintln!("Usage: {} <arg1> <arg2> <arg3>, excel file name, column header name, customer segment id are missing", args[0]);
         std::process::exit(1);
     }
 
     let file_path = &args[1];
-    let customer_segment_id = &args[2];
+    let column_header = &args[2];
+    let customer_segment_id = &args[3];
 
     // Create an empty vector
     let mut customer_numbers: Vec<i64> = Vec::new();
@@ -25,25 +26,66 @@ fn main() {
     // Open the Excel file
     let mut workbook: Xlsx<_> = open_workbook(file_path).expect("Error opening Excel file");
 
+    //let sheet = workbook.worksheet_range_at(0).unwrap().unwrap();
+
+    //let header_row = sheet.
+
     // Get the first worksheet
     if let Some(Ok(sheet)) = workbook.worksheet_range_at(0) {
-        // Iterate over rows
-        for row in sheet.rows() {
-            // Iterate over cells in the row
-            for cell in row.iter() {
-                match cell {
-                    Data::Float(cell) => {
 
-                        let num: i64 = *cell as i64;
-                        customer_numbers.push(num);
-                    }
-                    _ => {
-                        // Handle other data types if needed
-                        //println!("Not a customer number!!");
+        // Extract the headers from the first row
+        let headers: Vec<String> = sheet
+            .rows()
+            .next()
+            .unwrap()
+            .iter()
+            .map(|c| c.to_string())
+            .collect();
+
+        // Find the index of the specific column header
+        let column_index = headers.iter().position(|header| header == column_header);
+
+        if let Some(_index) = column_index {
+            // Iterate over rows, skipping the header row
+            for row in sheet.rows().skip(1) {
+                // Get the cell value from the specific column
+                for cell in row.iter() {
+                    match cell {
+                        Data::Float(cell) => {
+                            let num: i64 = *cell as i64;
+                            customer_numbers.push(num);
+                        }
+                        Data::Int(cell) => {
+                            customer_numbers.push(*cell);
+                        }
+                        _ => {
+                            // Handle other data types if needed
+                            //println!("Not a customer number!!");
+                        }
                     }
                 }
             }
+        } else {
+            println!("Column header not found");
         }
+
+        // Iterate over rows
+        // for row in sheet.rows() {
+        //     // Iterate over cells in the row
+        //     for cell in row.iter() {
+        //         match cell {
+        //             Data::Float(cell) => {
+
+        //                 let num: i64 = *cell as i64;
+        //                 customer_numbers.push(num);
+        //             }
+        //             _ => {
+        //                 // Handle other data types if needed
+        //                 //println!("Not a customer number!!");
+        //             }
+        //         }
+        //     }
+        // }
     }
 
     // Print the vector
